edb491408bb64add13068544a1fd27e13bc50334
Kimmo Sääskilahti <kimmo.saaskilahti@silo.ai>
2021-12-27 13:55:45 +0200

    First preprocessing.

--- README.md
+++ README.md
@@ -1,3 +1,45 @@
 # Bird call identification
 
 See [BirdCLEF 2021 - Birdcall Identification](https://www.kaggle.com/c/birdclef-2021#) Kaggle competition.
+
+## Getting started
+
+Install dependencies:
+
+```bash
+$ pip install -e '.[dev]'
+```
+
+### Setup `kaggle`
+
+Sign in to Kaggle. Follow the [instructions](https://github.com/Kaggle/kaggle-api) to prepare `~/.kaggle/kaggle.json` file.
+
+### Working with data
+
+See the [Data](https://www.kaggle.com/c/birdclef-2021/data) page.
+
+Download the 39 GiB dataset:
+
+```bash
+$ kaggle competitions download birdclef-2021 -p data
+```
+
+Download single file:
+
+```bash
+$ kaggle competitions download birdclef-2021 -p data/train_short_audio/acafly -f train_short_audio/acafly/XC109605.ogg
+```
+
+List all files in CSV format
+
+```bash
+$ kaggle competitions files birdclef-2021 --csv
+```
+
+### Setup Jupyter
+
+Install kernel:
+
+```bash
+$ python -m ipykernel install --user --name bird-3.8.1 --display-name "Python (bird-3.8.1)"
+```
---
+++ src/preprocess.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+import typing
+
+import pandas as pd
+import reverse_geocoder as rg
+
+DATA_FOLDER = Path("data")
+
+
+def add_country(df):
+    latlng = df[["latitude", "longitude"]].apply(tuple, axis=1)
+    places = rg.search(list(latlng))
+    countries = [place["cc"] for place in places]
+    return df.assign(country=countries)
+
+
+def filter_by_species_within_countries(
+    df: pd.DataFrame, countries: typing.Tuple[str] = ("FI",)
+):
+    with_country = add_country(df)
+
+    sightings_within = with_country[with_country["country"].isin(countries)]
+
+    species = set(sightings_within["scientific_name"].values)
+
+    filtered = df[df["scientific_name"].isin(species)]
+
+    assert len(filtered) > 0
+
+    return filtered
+
+
+def sample_small():
+    df = pd.read_csv(DATA_FOLDER.joinpath("train_metadata.csv"))
+
+    filtered = filter_by_species_within_countries(df)
+
+    out = DATA_FOLDER.joinpath("sampled").joinpath("train_metadata.csv")
+
+    out.parent.mkdir(exist_ok=True)
+
+    filtered.to_csv(DATA_FOLDER.joinpath("sampled").joinpath("train_metadata.csv"))
+
+
+if __name__ == "__main__":
+    sample_small()
